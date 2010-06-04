72f077016de78b4d8ac65878b8d33fb2f7b1cee2
Juarez Bochi <jbochi@gmail.com>
2010-06-04 17:50:29 -0300

    Path normalization + appstats config file

---
+++ src/appengine_config.py
@@ -0,0 +1,20 @@
+import re
+
+def webapp_add_wsgi_middleware(app):
+    from google.appengine.ext.appstats import recording
+    app = recording.appstats_wsgi_middleware(app)
+    return app
+
+def appstats_normalize_path(path):
+    if path.startswith('/ajax/stop_details/'):
+        return '/ajax/stop_details/<stop_id>/'
+    elif path.startswith('/ajax/get_poly/'):
+        return '/ajax/get_poly/<trip_id>/'
+    elif path.startswith('/lista') or path.startswith('/busca'):
+        return path
+    elif re.match('/[^/]+/[^/]+\.kml', path):
+        return '/<route_id>/<route_slug>.kml'
+    elif re.match('/[^/]+/[^/]+', path):
+        return '/<route_id>/<route_slug>/'
+    else:
+        return path
